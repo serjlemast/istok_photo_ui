a019c000e34313e7a89357e96d04b717104e9d94
SerJLemast <maksiomRpp@gmail.com>
2021-04-29 14:36:19 +0300

    create component for info

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="info">
-    <h1>Info</h1>
+    <el-tabs type="border-card">
+      <el-tab-pane label="User">
+        User
+        <h1>Statistic</h1>
+      </el-tab-pane>
+      <el-tab-pane label="Image management">
+        <el-tabs :tab-position="tabPosition" style="height: 200px">
+          <el-tab-pane label="Allow Image View">Allow Image View</el-tab-pane>
+          <el-tab-pane label="Upload image">Upload image</el-tab-pane>
+          <el-tab-pane label="Remove image">Remove image</el-tab-pane>
+        </el-tabs>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
 export default {
   name: "Info",
+  data() {
+    return {
+      tabPosition: "left",
+    };
+  },
 };
 </script>
 
