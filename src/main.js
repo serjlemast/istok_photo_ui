457b2bc971cd19ec21277c43462141b32da954eb
SerJLemast <maksiomRpp@gmail.com>
2021-04-29 14:37:03 +0300

    set up and use v-lightbox

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
 import router from './router'
+// import Lightbox from '@morioh/v-lightbox'
+
+// import '@morioh/v-lightbox/dist/lightbox.css';
+
+
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
+// Vue.use(Lightbox);
 
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
--- src/pages/Images.vue
+++ src/pages/Images.vue
@@ -1,13 +1,59 @@
 <template>
-  <div class="image">
-    <h1>Images</h1>
+  <div class="images">
+    <div class="gallery-row">
+      <div class="gallery-container">
+        <lightbox :items="images"></lightbox>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import lightbox from "../components/lightbox.vue";
+import "../components/lightbox.css";
+
 export default {
   name: "Images",
+  components: {
+    lightbox,
+  },
+  data() {
+    return {
+      images: [
+        "https://i.wifegeek.com/200426/f9459c52.jpg",
+        "https://i.wifegeek.com/200426/5ce1e1c7.jpg",
+        "https://i.wifegeek.com/200426/5fa51df3.jpg",
+        "https://i.wifegeek.com/200426/663181fe.jpg",
+        "https://i.wifegeek.com/200426/2d110780.jpg",
+        "https://i.wifegeek.com/200426/e73cd3fa.jpg",
+        "https://i.wifegeek.com/200426/15160d6e.jpg",
+        "https://i.wifegeek.com/200426/d0c881ae.jpg",
+        "https://i.wifegeek.com/200426/a154fc3d.jpg",
+        "https://i.wifegeek.com/200426/71d3aa60.jpg",
+        "https://i.wifegeek.com/200426/d17ce9a0.jpg",
+        "https://i.wifegeek.com/200426/7c4deca9.jpg",
+        "https://i.wifegeek.com/200426/64672676.jpg",
+        "https://i.wifegeek.com/200426/de6ab9c6.jpg",
+        "https://i.wifegeek.com/200426/d8bcb6a7.jpg",
+        "https://i.wifegeek.com/200426/4085d03b.jpg",
+        "https://i.wifegeek.com/200426/177ef44c.jpg",
+        "https://i.wifegeek.com/200426/d74d9040.jpg",
+        "https://i.wifegeek.com/200426/81e24a47.jpg",
+        "https://i.wifegeek.com/200426/43e2e8bb.jpg",
+      ],
+    };
+  },
 };
 </script>
 
-<style scoped>
+<style>
+.gallery-container {
+  height: 604.8px;
+  width: 750px;
+}
+
+.gallery-row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
