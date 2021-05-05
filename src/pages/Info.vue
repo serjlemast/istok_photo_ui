7db21a61163c0067db3ade4232adac71a86829e1
SerJLemast <maksiomRpp@gmail.com>
2021-05-05 14:29:42 +0300

    add charts

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="info">
     <el-tabs type="border-card">
-      <el-tab-pane label="User">
-        User
-        <h1>Statistic</h1>
+      <el-tab-pane label="Statistic">
+        <el-row type="flex" class="gallery-row">
+          <el-col :span="6" justify="!end">
+            <div class="grid-content">
+              <div class="small">
+                <bar-chart :chart-data="renderChart"></bar-chart>
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="6" justify="!end">
+            <div class="small">
+              <doughnut-chart :chart-data="renderChart"></doughnut-chart>
+            </div>
+          </el-col>
+        </el-row>
       </el-tab-pane>
       <el-tab-pane label="Image management">
         <el-tabs :tab-position="tabPosition" style="height: 200px">
@@ -16,15 +28,44 @@
   </div>
 </template>
 <script>
+import BarChart from "../components/BarChart.js";
+import DoughnutChart from "../components/DoughnutChart.js";
+
 export default {
+  components: {
+    BarChart,
+    DoughnutChart,
+  },
   name: "Info",
   data() {
     return {
       tabPosition: "left",
+      renderChart: {
+        labels: ["13", "10"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [24, 17],
+          },
+          {
+            label: "Data two",
+            backgroundColor: "#f0000",
+            data: [25, 37],
+          },
+        ],
+        responsive: true,
+        maintainAspectRatio: false,
+      },
     };
   },
 };
 </script>
 
-<style scoped>
+<style>
+.small {
+  max-width: 300px;
+  min-width: 150px;
+  margin: 20px auto;
+}
 </style>
